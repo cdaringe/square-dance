2adfe904735412ebba5960fb70a028ca9c6311b7
cdaringe <cdieringer@mrn.org>
2015-08-20 18:42:09 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import Banana from './banana'
 export default class App extends Component {
   render() {
     return (
+        <div>
       <h1>Hello, world.</h1>
+      <Banana></Banana>
+      </div>
     );
   }
 }
---
+++ src/banana.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class Banana extends Component {
+  render() {
+    return (
+      <div> ya ya ya </div>
+    );
+  }
+}
