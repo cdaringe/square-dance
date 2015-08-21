744476a3779d4e5c7e4bae52387afd99b99fb422
drewlandis <dlandis@mrn.org>
2015-08-21 15:07:57 -0600

    add FireFox compatibility

--- src/dancefloor.js
+++ src/dancefloor.js
@@ -15,17 +15,34 @@ export default class Dancefloor extends React.Component {
             }, 100); // css transition delay
             let dx = 0;
             let dy = 0;
-            switch (evt.keyIdentifier.toLowerCase()) {
-                case 'up':
-                    dy += 10; break;
-                case 'down':
-                    dy -= 10; break;
-                case 'left':
-                    dx -= 10; break;
-                case 'right':
-                    dx += 10; break;
-                default:
-                    break;
+            if (evt.keyIdentifier !== undefined) {
+                // Chrome and ...?
+                switch (evt.keyIdentifier.toLowerCase()) {
+                    case 'up':
+                        dy += 10; break;
+                    case 'down':
+                        dy -= 10; break;
+                    case 'left':
+                        dx -= 10; break;
+                    case 'right':
+                        dx += 10; break;
+                    default:
+                        break;
+               }
+            } else if (evt.key !== undefined) {
+                // play nicely with Firefox
+                switch (evt.key.toLowerCase()) {
+                    case 'arrowup':
+                        dy += 10; break;
+                    case 'arrowdown':
+                        dy -= 10; break;
+                    case 'arrowleft':
+                        dx -= 10; break;
+                    case 'arrowright':
+                        dx += 10; break;
+                    default:
+                        break;
+                }
             }
             this.props.move({ dx, dy });
         });
