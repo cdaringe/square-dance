eb06bafcf960f531f04b6782a93ca1d67de8b4e0
Cory Reed <creed@mrn.org>
2015-08-20 20:24:13 -0700

    Wire up square buttons to 'move' method :baby:

--- src/square.js
+++ src/square.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-    constructor() {
-        super();
-
-        this.move.bind(this);
-    }
     move(direction) {
         console.log('Square moved:', direction)
     }
@@ -13,16 +8,16 @@ export default class Square extends React.Component {
         return (
             <div className="square">
                 <button
-                    onClick={this.move('up')}
+                    onClick={this.move.bind(this, 'up')}
                     className="square__arrow--up">&uarr;</button>
                 <button
-                    onClick={this.move('right')}
+                    onClick={this.move.bind(this, 'right')}
                     className="square__arrow--right">&rarr;</button>
                 <button
-                    onClick={this.move('down')}
+                    onClick={this.move.bind(this, 'down')}
                     className="square__arrow--down">&darr;</button>
                 <button
-                    onClick={this.move('left')}
+                    onClick={this.move.bind(this, 'left')}
                     className="square__arrow--left">&larr;</button>
             </div>
         );
