44e11c61e8e8e61e29f15cc1d1ed42f2cc9c94fc
cdaringe <cdieringer@mrn.org>
2015-08-20 20:00:04 -0700

    [5mSO MUCH HACK[0m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Banana from './banana'
+import { Provider } from 'react-redux';
 export default class App extends Component {
   render() {
     return (
-        <div>
-      <h1>Hello, world.</h1>
-      <Banana></Banana>
-      </div>
+        <Provider store={store}>
+            {() => <h1>Hello, world.</h1>}
+        </Provider>
     );
   }
 }
+        // <h1>Hello, world.</h1>
--- src/banana.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-export default class Banana extends Component {
-  render() {
-    return (
-      <div> ya ya ya </div>
-    );
-  }
-}
