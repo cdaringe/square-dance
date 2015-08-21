53a63b836caeb307b340903d5418d6b2d05e4a61
cdaringe <cdieringer@mrn.org>
2015-08-21 10:55:51 -0700

    such static build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 var path = require('path');
 var webpack = require('webpack');
+var isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
@@ -26,13 +25,14 @@ module.exports = {
       loaders: ['react-hot', 'babel'],
       exclude: /node_modules/
     }, {
-    //   test: /\.js$/,
-    //   loaders: ['babel'],
-    //   include: path.join(__dirname)
-    // }, {
     test: /\.css?$/,
     loaders: ['style', 'css'],
     include: path.join(__dirname, 'styles')
   }]
   }
 };
+
+if (isDev) {
+    module.exports.entry.push('webpack-dev-server/client?http://localhost:3000');
+    module.exports.entry.push('webpack/hot/only-dev-server');
+}
